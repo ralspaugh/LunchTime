e1a1906109327fdcc2be7b90f47a5e1b745c80f7
Ryan Alspaugh <ryan.alspaugh@slalom.com>
2016-01-05 14:18:44 -0600

    Ignore AuthenticatedClient.scala

--- src/main/scala/com/ralspaugh/learning/scala/rest/client/AuthenticatedClient.scala
+++ src/main/scala/com/ralspaugh/learning/scala/rest/client/AuthenticatedClient.scala
@@ -4,6 +4,6 @@ trait AuthenticatedClient {
 
   // These values should never be checked into source control, which is why this trait is git ignored.
   // Replace your local copy with the clientId and clientSecret provided to you from Foursquare.
-  val clientId = ""
+  val clientId = "test"
   val clientSecret = ""
 }
